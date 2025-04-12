e6a277a517ce345966df546e9f572cc7aa3bff7c
Rishi-S20 <rishisuryavanshi14@gmail.com>
2025-04-12 10:09:11 -0500

    font maybe

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Satoshi: ["Satoshi", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
